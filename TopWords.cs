b08399b4eea0f954d79664dad1018ee1a4f410be
Yoav Dobrin <yodobrin@microsoft.com>
2021-10-27 17:05:41 +0300

    initial code

--- TopWords.cs
+++ TopWords.cs
@@ -17,7 +17,7 @@ namespace know
     {
         [FunctionName("TopWords")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("TopWords function processed a request.");       
